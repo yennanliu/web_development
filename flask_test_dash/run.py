e93bb9434a0ea2093aae48c77581e1e204c409f0
yennanliu <f339339@gmail.com>
2016-11-04 10:15:47 +0800

    Add files via upload

--- flask_test_dash/run.py
+++ flask_test_dash/run.py
@@ -36,6 +36,17 @@ def hello():
 
 
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/all_data')
 # ref https://sarahleejane.github.io/learning/python/2015/08/09/simple-tables-in-webapps-using-flask-and-pandas-with-python.html
 def all_():
