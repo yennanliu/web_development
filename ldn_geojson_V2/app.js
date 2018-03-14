09b44e1b6210bb50aad408e8e45ff0285ef1aba1
yennanliu <f339339@gmail.com>
2018-03-14 16:46:37 +0000

    beauty the JS layout

--- ldn_geojson_V2/app.js
+++ ldn_geojson_V2/app.js
@@ -1,95 +1,95 @@
 var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', function($scope, $http) {
-  vm = this;
+    vm = this;
 
-  // Set the Map Options to be applied when the map is set.
-  var mapOptions = {
-    zoom: 10,
-    scrollwheel: false,
-    center: new google.maps.LatLng(51.5074, 0.1278),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    mapTypeControl: false,
-    mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.TERRAIN]
+    // Set the Map Options to be applied when the map is set.
+    var mapOptions = {
+        zoom: 10,
+        scrollwheel: false,
+        center: new google.maps.LatLng(51.5074, 0.1278),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.TERRAIN]
+        }
     }
-  }
 
-  // Set a blank infoWindow to be used for each to state on click
-  var infoWindow = new google.maps.InfoWindow({
-    content: ""
-  });
-
-  // Set the map to the element ID and give it the map options to be applied
-  vm.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  // Create the state data layer and load the GeoJson Data
-  var stateLayer = new google.maps.Data();
-  //stateLayer.loadGeoJson('https://gist.githubusercontent.com/dmarg/b2959e771ae680acbc95/raw/815a03f55d028dace4371c27d0b787ca0f2f2b5d/states.json');
-  stateLayer.loadGeoJson('ldn.geo.json');
-  // Set and apply styling to the stateLayer
-  stateLayer.setStyle(function(feature) {
-    console.log(feature['id'])
-    console.log(feature.getProperty('id'))
-    return {
-      fillColor: getColor(feature.getProperty('id')),  // call function to get color for state based on the COLI (Cost of Living Index)
-      fillOpacity: 0.8,
-      strokeColor: '#b3b3b3',
-      strokeWeight: 1,
-      zIndex: 1
-    };
-  });
-
-  // Add mouseover and mouse out styling for the GeoJSON State data
-  stateLayer.addListener('mouseover', function(e) {
-    stateLayer.overrideStyle(e.feature, {
-      strokeColor: '#2a2a2a',
-      strokeWeight: 2,
-      zIndex: 2
+    // Set a blank infoWindow to be used for each to state on click
+    var infoWindow = new google.maps.InfoWindow({
+        content: ""
     });
-  });
 
-  stateLayer.addListener('mouseout', function(e) {
-    stateLayer.revertStyle();
-  });
+    // Set the map to the element ID and give it the map options to be applied
+    vm.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  // Adds an info window on click with in a state that includes the state name and COLI
-  stateLayer.addListener('click', function(e) {
-    console.log(e);
-    console.log(e.feature.f);
-    infoWindow.setContent(
-      '<div style="line-height:1.00;overflow:hidden;white-space:nowrap;">' +
-      'Zone : <br>' +
-      '<br> gid: ' + e.feature.f.gid + 
-      '<br> geom: ' + e.feature.f.geom  +
-      '</div>'
-      );
+    // Create the state data layer and load the GeoJson Data
+    var stateLayer = new google.maps.Data();
+    //stateLayer.loadGeoJson('https://gist.githubusercontent.com/dmarg/b2959e771ae680acbc95/raw/815a03f55d028dace4371c27d0b787ca0f2f2b5d/states.json');
+    stateLayer.loadGeoJson('ldn.geo.json');
+    // Set and apply styling to the stateLayer
+    stateLayer.setStyle(function(feature) {
+        console.log(feature['id'])
+        console.log(feature.getProperty('id'))
+        return {
+            fillColor: getColor(feature.getProperty('id')), // call function to get color for state based on the COLI (Cost of Living Index)
+            fillOpacity: 0.8,
+            strokeColor: '#b3b3b3',
+            strokeWeight: 1,
+            zIndex: 1
+        };
+    });
 
-    var anchor = new google.maps.MVCObject();
-    anchor.set("position", e.latLng);
-    infoWindow.open(vm.map, anchor);
-  });
+    // Add mouseover and mouse out styling for the GeoJSON State data
+    stateLayer.addListener('mouseover', function(e) {
+        stateLayer.overrideStyle(e.feature, {
+            strokeColor: '#2a2a2a',
+            strokeWeight: 2,
+            zIndex: 2
+        });
+    });
+
+    stateLayer.addListener('mouseout', function(e) {
+        stateLayer.revertStyle();
+    });
+
+    // Adds an info window on click with in a state that includes the state name and COLI
+    stateLayer.addListener('click', function(e) {
+        console.log(e);
+        console.log(e.feature.f);
+        infoWindow.setContent(
+            '<div style="line-height:1.00;overflow:hidden;white-space:nowrap;">' +
+            'Zone : <br>' +
+            '<br> gid: ' + e.feature.f.gid +
+            '<br> geom: ' + e.feature.f.geom +
+            '</div>'
+        );
+
+        var anchor = new google.maps.MVCObject();
+        anchor.set("position", e.latLng);
+        infoWindow.open(vm.map, anchor);
+    });
 
 
-  // Final step here sets the stateLayer GeoJSON data onto the map
-  stateLayer.setMap(vm.map);
+    // Final step here sets the stateLayer GeoJSON data onto the map
+    stateLayer.setMap(vm.map);
 
 
-  // returns a color based on the value given when the function is called
-  function getColor(coli) {
-    var colors = [
-      '#d1ccad',
-      '#c2c083',
-      '#cbd97c',
-      '#acd033',
-      '#89a844'
-    ];
+    // returns a color based on the value given when the function is called
+    function getColor(coli) {
+        var colors = [
+            '#d1ccad',
+            '#c2c083',
+            '#cbd97c',
+            '#acd033',
+            '#89a844'
+        ];
 
-    return coli >= 10 ? colors[4] :
-      coli > 15 ? colors[3] :
-      coli > 20 ? colors[2] :
-      coli > 25 ? colors[1] :
-      colors[0];
-  }
+        return coli >= 10 ? colors[4] :
+            coli > 15 ? colors[3] :
+            coli > 20 ? colors[2] :
+            coli > 25 ? colors[1] :
+            colors[0];
+    }
 
 });
