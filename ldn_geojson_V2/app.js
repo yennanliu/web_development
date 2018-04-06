dd314b0a141663db5281e1eb72ca2d45696848f3
yennanliue <f339339@gmail.com>
2018-04-06 14:05:38 +0100

    modify map style

--- ldn_geojson_V2/app.js
+++ ldn_geojson_V2/app.js
@@ -10,6 +10,7 @@ app.controller('MainCtrl', function($scope, $http) {
         center: new google.maps.LatLng(51.5074, 0.1278),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         mapTypeControl: false,
+        styles: [{"featureType":"road","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"weight":1}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"weight":0.8}]},{"featureType":"landscape","stylers":[{"color":"#ffffff"}]},{"featureType":"water","stylers":[{"visibility":"off"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"elementType":"labels","stylers":[{"visibility":"off"}]},{"elementType":"labels.text","stylers":[{"visibility":"on"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#000000"}]},{"elementType":"labels.icon","stylers":[{"visibility":"on"}]}],
         mapTypeControlOptions: {
             mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.TERRAIN]
         }
