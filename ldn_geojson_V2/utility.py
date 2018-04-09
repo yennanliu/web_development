614642adc9309d7b3467e994a204b408cec3f879
yennanliu <f339339@gmail.com>
2018-04-09 13:57:24 +0100

    fix sqlite output data to 2-D array

--- ldn_geojson_V2/utility.py
+++ ldn_geojson_V2/utility.py
@@ -13,20 +13,18 @@ def csv_2_array(file):
 	return your_list
 
 
-
-
 def sqlite_2_array(db_name):
-	output=[]
-	conn = sqlite3.connect(db_name)
-	c = conn.cursor()
-	sql_get_car_date = """SELECT * FROM car_data"""
-	print (sql_get_car_date)
-	for row in c.execute(sql_get_car_date):
-		print (row)
-		output.append(row)
-	print ('------------')
-	print (output)
-	return output
+    conn = sqlite3.connect('car_data.db')
+    #conn.row_factory = lambda cursor, row: [row[0],row[1]]
+    conn.row_factory = lambda cursor, row: [row[x] for x in range(5)]
+    c = conn.cursor()
+    sql_get_car_date = """SELECT * FROM car_data"""
+    print (sql_get_car_date)
+    # tranform python sqlite3 output from  tuple to array 
+    # https://stackoverflow.com/questions/2854011/get-a-list-of-field-values-from-pythons-sqlite3-not-tuples-representing-rows
+    output = list(c.execute(sql_get_car_date).fetchall())
+    print (output)
+    return output
 
 
 
