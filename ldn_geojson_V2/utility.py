557a91658fdd47bd2de014e9465fcb4e23bac964
yennanliu <f339339@gmail.com>
2018-04-09 13:19:30 +0100

    add sqlite data extract function

--- ldn_geojson_V2/utility.py
+++ ldn_geojson_V2/utility.py
@@ -2,7 +2,7 @@
 
 
 import csv
-
+import sqlite3
 
 def csv_2_array(file):
 	with open(file, 'r') as f:
@@ -15,6 +15,29 @@ def csv_2_array(file):
 
 
 
+def sqlite_2_array(db_name):
+	output=[]
+	conn = sqlite3.connect(db_name)
+	c = conn.cursor()
+	sql_get_car_date = """SELECT * FROM car_data"""
+	print (sql_get_car_date)
+	for row in c.execute(sql_get_car_date):
+		print (row)
+		output.append(row)
+	print ('------------')
+	print (output)
+	return output
+
+
+
 
 if __name__ == '__main__':
-	csv_2_array('car_data.csv')
+	#csv_2_array('car_data.csv')
+	sqlite_2_array('car_data.db')
+
+
+
+
+
+
+
