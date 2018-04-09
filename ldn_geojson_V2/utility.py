ac3cb03ca776f7687bde3b27498889572c5d9ff4
yennanliu <f339339@gmail.com>
2018-04-09 14:00:13 +0100

    fix code layout

--- ldn_geojson_V2/utility.py
+++ ldn_geojson_V2/utility.py
@@ -14,18 +14,18 @@ def csv_2_array(file):
 
 
 def sqlite_2_array(db_name):
-    conn = sqlite3.connect('car_data.db')
-    #conn.row_factory = lambda cursor, row: [row[0]]
-    # tranform python sqlite3 output from  tuple to array 
+	conn = sqlite3.connect('car_data.db')
+	#conn.row_factory = lambda cursor, row: [row[0]]
+	# tranform python sqlite3 output from  tuple to array 
 	conn.row_factory = lambda cursor, row: [row[x] for x in range(5)]
-    c = conn.cursor()
-    sql_get_car_date = """SELECT * FROM car_data"""
-    print (sql_get_car_date)
-    # tranform python sqlite3 output from  tuple to array 
-    # https://stackoverflow.com/questions/2854011/get-a-list-of-field-values-from-pythons-sqlite3-not-tuples-representing-rows
-    output = list(c.execute(sql_get_car_date).fetchall())
-    print (output)
-    return output
+	c = conn.cursor()
+	sql_get_car_date = """SELECT * FROM car_data"""
+	print (sql_get_car_date)
+	# tranform python sqlite3 output from  tuple to array 
+	# https://stackoverflow.com/questions/2854011/get-a-list-of-field-values-from-pythons-sqlite3-not-tuples-representing-rows
+	output = list(c.execute(sql_get_car_date).fetchall())
+	print (output)
+	return output
 
 
 
