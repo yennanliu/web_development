8d7dd56df15cc4f5dfee3c7d7c89d5273a3af66f
yennanliu <f339339@gmail.com>
2018-04-11 12:26:33 +0100

    try testing geojson dataset

--- ldn_geojson_V2/app.js
+++ ldn_geojson_V2/app.js
@@ -28,7 +28,8 @@ app.controller('MainCtrl', function($scope, $http) {
     var stateLayer = new google.maps.Data();
     //stateLayer.loadGeoJson('https://gist.githubusercontent.com/dmarg/b2959e771ae680acbc95/raw/815a03f55d028dace4371c27d0b787ca0f2f2b5d/states.json');
     //stateLayer.loadGeoJson('ldn.geo.json');
-    stateLayer.loadGeoJson('london_postcodes.geo.json');
+    //stateLayer.loadGeoJson('london_postcodes.geo.json');
+    stateLayer.loadGeoJson('ldn_hz.geo.json');
     // Set and apply styling to the stateLayer
     stateLayer.setStyle(function(feature) {
         //console.log(feature['id'])
