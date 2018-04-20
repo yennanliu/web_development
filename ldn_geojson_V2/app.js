901809a58e6d2e00deb201ee8fb28603006650d6
yennanliue <f339339@gmail.com>
2018-04-20 16:25:04 +0100

    roll back V2 from copied V3

--- ldn_geojson_V2/app.js
+++ ldn_geojson_V2/app.js
@@ -28,8 +28,9 @@ app.controller('MainCtrl', function($scope, $http) {
     var stateLayer = new google.maps.Data();
     //stateLayer.loadGeoJson('https://gist.githubusercontent.com/dmarg/b2959e771ae680acbc95/raw/815a03f55d028dace4371c27d0b787ca0f2f2b5d/states.json');
     //stateLayer.loadGeoJson('ldn.geo.json');
-    stateLayer.loadGeoJson('london_postcodes.geo.json');
+    //stateLayer.loadGeoJson('london_postcodes.geo.json');
     //stateLayer.loadGeoJson('ldn_hz.geo.json');
+    stateLayer.loadGeoJson('ldn_fl_hhz.geo.json');
     // Set and apply styling to the stateLayer
     stateLayer.setStyle(function(feature) {
         //console.log(feature['id'])
@@ -233,3 +234,17 @@ app.controller('MainCtrl', function($scope, $http) {
 
 
 });
+
+
+app.controller('siteCtrl', function($scope, $http) {
+    $http({
+        method: 'GET',
+        url: ' car_data.json'
+    }).then(function successCallback(response) {
+            $scope.names = response.data.sites;
+        }, function errorCallback(response) {
+           // run above code when request fail
+    });      
+});
+
+
