e5e42c7f7c1aed10308462bd305ef9b61c0f1cb5
yennanliu <f339339@gmail.com>
2018-08-22 11:02:36 +0100

    fix code layout

--- flask_update_DB_tool/controller.py
+++ flask_update_DB_tool/controller.py
@@ -33,40 +33,41 @@ def get_DB_data(sql, db_url):
 
 
 def get_access_request_report(memeber_id,db_url):
-    # xlsxwriter write multiple dataframe to one xls
-    # https://stackoverflow.com/questions/32957441/putting-many-python-pandas-dataframes-to-one-excel-worksheet
-    # sql
-    sql_member_inform="""select * from ana.members where member_id = '{}' ;"""
-    sql_member_inform = sql_member_inform.format(memeber_id)
-    print (sql_member_inform)
-    sql_membership_event ="""select * from ana.membership_events where member_id = '{}' ;"""
-    sql_membership_event = sql_membership_event.format(memeber_id)
-    print (sql_membership_event)
-    sql_member_booking="""select * from ana.bookings where member_id = '{}' ;"""
-    sql_member_booking = sql_member_booking.format(memeber_id)
-    print (sql_member_booking)
-    sql_member_trips="""select * from prc.trips where member_id = '{}' ;"""
-    sql_member_trips = sql_member_trips.format(memeber_id)
-    print (sql_member_trips)
-    sql_member_transactions="""select * from prc.transactions where member_id = '{}' ;"""
-    sql_member_transactions = sql_member_transactions.format(memeber_id)
-    print (sql_member_transactions)
-    # extract data
-    df_member_inform= get_DB_data(sql_member_inform ,db_url)
-    df_membership_event= get_DB_data(sql_membership_event ,db_url)
-    df_member_booking= get_DB_data(sql_member_booking ,db_url)
-    df_member_trips= get_DB_data(sql_member_trips ,db_url)
-    df_member_transactions= get_DB_data(sql_member_transactions ,db_url)
-    # create xls writer
-    writer = pd.ExcelWriter('access_req_report_sample.xlsx',engine='xlsxwriter')  
-    workbook=writer.book
-    # save to excel xlsx file 
-    df_member_inform.to_excel(writer,sheet_name='df_member_inform',startrow=0 , startcol=0)  
-    df_membership_event.to_excel(writer,sheet_name='df_membership_event',startrow=0, startcol=0)
-    df_member_booking.to_excel(writer,sheet_name='df_member_booking',startrow=0, startcol=0)
-    df_member_trips.to_excel(writer,sheet_name='df_member_trips',startrow=0, startcol=0)
-    df_member_transactions.to_excel(writer,sheet_name='df_member_transactions',startrow=0, startcol=0)
-    #return writer 
+	# xlsxwriter write multiple dataframe to one xls
+	# https://stackoverflow.com/questions/32957441/putting-many-python-pandas-dataframes-to-one-excel-worksheet
+	sql_member_inform="""select * from ana.members where member_id = '{}' ;"""
+	sql_member_inform = sql_member_inform.format(memeber_id)
+	print (sql_member_inform)
+	sql_membership_event ="""select * from ana.membership_events where member_id = '{}' ;"""
+	sql_membership_event = sql_membership_event.format(memeber_id)
+	print (sql_membership_event)
+	sql_member_booking="""select * from ana.bookings where member_id = '{}' ;"""
+	sql_member_booking = sql_member_booking.format(memeber_id)
+	print (sql_member_booking)
+	sql_member_trips="""select * from prc.trips where member_id = '{}' ;"""
+	sql_member_trips = sql_member_trips.format(memeber_id)
+	print (sql_member_trips)
+	sql_member_transactions="""select * from prc.transactions where member_id = '{}' ;"""
+	sql_member_transactions = sql_member_transactions.format(memeber_id)
+	print (sql_member_transactions)
+	# extract data
+	df_member_inform= get_DB_data(sql_member_inform ,db_url)
+	df_membership_event= get_DB_data(sql_membership_event ,db_url)
+	df_member_booking= get_DB_data(sql_member_booking ,db_url)
+	df_member_trips= get_DB_data(sql_member_trips ,db_url)
+	df_member_transactions= get_DB_data(sql_member_transactions ,db_url)
+	# create xls writer
+	writer = pd.ExcelWriter('access_req_report_sample.xlsx',engine='xlsxwriter')  
+	workbook=writer.book
+	# save to excel xlsx file 
+	df_member_inform.to_excel(writer,sheet_name='df_member_inform',startrow=0 , startcol=0)  
+	df_membership_event.to_excel(writer,sheet_name='df_membership_event',startrow=0, startcol=0)
+	df_member_booking.to_excel(writer,sheet_name='df_member_booking',startrow=0, startcol=0)
+	df_member_trips.to_excel(writer,sheet_name='df_member_trips',startrow=0, startcol=0)
+	df_member_transactions.to_excel(writer,sheet_name='df_member_transactions',startrow=0, startcol=0)
+	#return writer 
+
+
 
 
 
