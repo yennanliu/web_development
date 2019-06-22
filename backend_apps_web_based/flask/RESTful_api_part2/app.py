22e228f965c04f616f23aa35ad4a18dd395265c6
yennanliu <f339339@gmail.com>
2019-06-22 16:53:33 +0800

    remove no need module imports, db define code

--- backend_apps_web_based/flask/RESTful_api_part2/app.py
+++ backend_apps_web_based/flask/RESTful_api_part2/app.py
@@ -1,14 +1,8 @@
 from flask import Flask, jsonify, abort, make_response, request 
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 import sqlite3
 import os
 
 app = Flask(__name__)
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud_sqlite')
-# db = SQLAlchemy(app)
-# ma = Marshmallow(app)
 
 # flask hello world 
 @app.route('/')
