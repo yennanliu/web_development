0eb5b5620e1786ed26392b0b462448fb8b5dd370
yennanliue <f339339@gmail.com>
2019-07-02 17:56:19 +0800

    add script init flask db

---
+++ backend_apps_web_based/flask/RESTful_api_part4/config.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
+Migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+class Product(db.Model):
+	__tablename__ = 'products'
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(64), unique=False, primary_key=False)
+	description = db.Column(db.String(128), unique=False, primary_key=False)
+	sold = db.Column(db.String(64), unique=False, primary_key=False)
+
+if __name__ == '__main__':
+	manager.run()
