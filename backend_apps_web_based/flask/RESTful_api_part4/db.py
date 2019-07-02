85159ef31d0c6b18a19b74e50f43f13f657e4290
yennanliue <f339339@gmail.com>
2019-07-02 18:32:52 +0800

    update insert data (via list of dict) (db.py)

--- backend_apps_web_based/flask/RESTful_api_part4/db.py
+++ backend_apps_web_based/flask/RESTful_api_part4/db.py
@@ -1,11 +1,21 @@
 from model import *
 
+# define to-insert db (will do that via sql/shell in the next update maybe)
+prod_data = [ {'title': 'juice',
+			 'description':'this is an apple juice',
+			 'sold':'True'}, 
+			  {'title': 'milk',
+			 'description':'this is a milk',
+			 'sold':'False'},
+			  {'title': 'salad',
+			 'description':'this is a salad',
+			 'sold':'True'}]
+             
 # insert data into db 
-title = 'juice' 
-description = 'this is an apple juice'
-sold = 'True'
-
-insert_data = ProductData(title=title, description=description, sold=sold)
-db.session.add(insert_data)
-db.session.commit()
-print ('insert data to DB ok')
+for data in prod_data:
+	insert_data = ProductData(title=data['title'], 
+							  description=data['description'], 
+							  sold=data['sold'])
+	db.session.add(insert_data)
+	db.session.commit()
+	print ('insert data to DB ok')
