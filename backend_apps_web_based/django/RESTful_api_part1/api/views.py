2b5c3f611d37e9c3716ecfe2e86db709cc64fa1a
yennanliu <f339339@gmail.com>
2019-07-09 15:29:24 +0800

    update api/views.py

--- backend_apps_web_based/django/RESTful_api_part1/api/views.py
+++ backend_apps_web_based/django/RESTful_api_part1/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics 
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
 
 # Create your views here.
+class CreateView(generics.ListCreateAPIView):
+	queryset = Bucketlist.objects.all()
+	serializers_class = BucketlistSerializer
+
+	def perform_create(self, serializers):
+		serializers.save()
