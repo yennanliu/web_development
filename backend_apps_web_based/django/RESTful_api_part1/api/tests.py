ac0ec3681a44f32bad72140792652e373bca5864
yennanliu <f339339@gmail.com>
2019-07-09 15:29:15 +0800

    update api/tests.py

--- backend_apps_web_based/django/RESTful_api_part1/api/tests.py
+++ backend_apps_web_based/django/RESTful_api_part1/api/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
 from .models import Bucketlist 
+from rest_framework.test import APIClient 
+from rest_framework import status 
+from django.core.urlresolvers import reverse 
 
 # Create your tests here.
 class ModelTestCase(TestCase):
 
 	def setUp(self):
-		self.bucketlist_name = "my bucketlistn"
+		self.bucketlist_name = "my bucketlist"
 		self.bucketlist = Bucketlist(name=self.bucketlist_name)
 
 	def test_model_can_create_a_bucketlist(self):
@@ -13,3 +16,16 @@ class ModelTestCase(TestCase):
 		self.bucketlist.save()
 		new_count = Bucketlist.objects.count()
 		self.assertNotEqual(old_count, new_count)
+
+class viewTestCase(TestCase):
+
+	def setUp(self):
+		self.client = APIClient()
+		self.bucketlist_data = {'name': 'go to NYC'}
+		self.response = self.client.post(
+			reverse('create'),
+			self.bucketlist_data,
+			format="json")
+
+	def test_api_can_create_a_bucketlist(self):
+		self.assertEqual(self.response.status_code,status.HTTP_201_CREATED)
