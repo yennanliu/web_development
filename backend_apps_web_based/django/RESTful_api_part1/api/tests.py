5f4f28a346c06636a8bdf98e48bd8bc236fac9e2
yennanliu <f339339@gmail.com>
2019-07-09 14:52:25 +0800

    add test for django app

--- backend_apps_web_based/django/RESTful_api_part1/api/tests.py
+++ backend_apps_web_based/django/RESTful_api_part1/api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Bucketlist 
 
 # Create your tests here.
+class ModelTestCase(TestCase):
+
+	def setUp(self):
+		self.bucketlist_name = "my bucketlistn"
+		self.bucketlist = Bucketlist(name=self.bucketlist_name)
+
+	def test_model_can_create_a_bucketlist(self):
+		old_count = Bucketlist.objects.count()
+		self.bucketlist.save()
+		new_count = Bucketlist.objects.count()
+		self.assertNotEqual(old_count, new_count)
